5321aa8994d12ed666d1416961181535f52b34b5
milletgilmore <milletgilmore@gmail.com>
2023-02-16 08:40:34 -0600

    signin screen design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Link, Route } from 'react-router-dom';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
   const cart = useSelector((state) => state.cart);
@@ -32,6 +33,7 @@ function App() {
         <Routes>
           <Route path="/cart/:id?" element={<CartScreen/>}></Route>
           <Route path="/product/:id" element={<ProductScreen/>}></Route>
+          <Route path="/signin" element={<SigninScreen/>}></Route>
           <Route path="/" element={<HomeScreen/>} exact></Route>
         </Routes>
       </main>

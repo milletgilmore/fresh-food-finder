f983f7334887d336c549d7277eef9bae55b8349a
milletgilmore <milletgilmore@gmail.com>
2023-02-16 07:53:10 -0600

    connect mongodb to products

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import data from './data.js';
+import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
 
 const app = express();
@@ -23,6 +23,7 @@ app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
 app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });

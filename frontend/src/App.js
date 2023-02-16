d56f1cdd1cc133ea54bb3b3887288abdd09a4413
milletgilmore <milletgilmore@gmail.com>
2023-02-16 07:00:33 -0600

    created cart screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 
@@ -20,6 +21,7 @@ function App() {
       </header>
       <main> 
         <Routes>
+          <Route path="/cart/:id?" element={<CartScreen/>}></Route>
           <Route path="/product/:id" element={<ProductScreen/>}></Route>
           <Route path="/" element={<HomeScreen/>} exact></Route>
         </Routes>

6ca5d5367a9f83ff0480c77aaa02e8f6e773931f
milletgilmore <milletgilmore@gmail.com>
2023-02-15 18:49:45 -0600

    day 2, added product page and home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import Product from './components/Product';
-import data from './data';
+import { Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 function App() {
   return (
+    
     <div className="grid-container">
       <header className="row">
         <div>
@@ -15,18 +18,15 @@ function App() {
           <a href="/signin">Sign In</a>
         </div>
       </header>
-      <main>
-        <div>
-          <div className="row center">
-            {data.products.map((product) => (
-              <Product key={product._id} product={product}></Product>
-            ))}
-          </div>
-        </div>
+      <main> 
+        <Routes>
+          <Route path="/product/:id" element={<ProductScreen/>} exact></Route>
+          <Route path="/" element={<HomeScreen/>} exact></Route>
+        </Routes>
       </main>
       <footer className="row center">All right reserved</footer>
     </div>
-    
+  
   );
 }
 
